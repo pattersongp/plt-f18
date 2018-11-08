9fd06317e140ed289c234fc3804d494cd164c8fb
Oi I Chan <oiichan@dyn-129-236-226-153.dyn.columbia.edu>
2018-11-08 17:20:34 -0500

    change all func to main and add void as nothing to return

---
+++ src/_build/ast.ml
@@ -0,0 +1,140 @@
+(* Abstract Syntax Tree *)
+
+type op = Plus | Minus | Times | Divide | Eq | Neq | Lt | Lteq | Gt | Gteq |
+          And | Or  | Req
+
+type uop = Neg | Not
+
+type mode = Read | Write | WriteRead
+
+type typ =
+          Int | Bool | Void | String | Array of typ * typ
+        | Function | File of mode | Regx
+
+type expr =
+    Literal of int
+  | BoolLit of bool
+  | StringLit of string
+  | Id of string
+  | Binop of expr * op * expr
+  | Unop of uop * expr
+  | Assign of string * expr
+  | Retrieve of string * expr
+  | Array_Assign of string * expr * expr
+  | Call of string * expr option list
+
+type bind = typ * string * expr option
+
+type stmt =
+    Block of stmt list
+  | Expr of expr
+  | Return of expr option
+  | If of expr * stmt * stmt
+  | While of expr * stmt
+  | For of typ * string * string * stmt
+  | Map of string * string
+  | Filter of string * string
+  | Open of string * string
+  | Break
+
+type func_decl = {
+    typ : typ;
+    fname : string;
+    formals : bind list;
+    locals : bind list;
+    body : stmt list;
+  }
+
+type program = bind list * func_decl list
+
+let string_of_uop = function
+          Neg -> "-"
+        | Not -> "!"
+
+let string_of_mode = function
+          Read -> "r"
+        | Write -> "w"
+        | WriteRead -> "wr"
+
+let rec string_of_typ = function
+          Int -> "int"
+        | String -> "str"
+        | Bool -> "bool"
+        | Array(t1, t2) -> "array" ^ "[" ^ string_of_typ t1 ^ ", " ^
+                string_of_typ t2 ^ "]"
+        | Void -> "void"
+        | Function -> "func"
+        | Regx -> "regx"
+        | File(mode) -> "file[" ^ string_of_mode mode ^ "]"
+
+let string_of_op = function
+          Plus  -> "+"
+        | Minus -> "-"
+        | Times -> "*"
+        | Divide-> "/"
+        | Eq    -> "=="
+        | Neq   -> "!="
+        | Lt    -> "<"
+        | Lteq  -> "<="
+        | Gt    -> ">"
+        | Gteq  -> ">="
+        | Req   -> "==="
+        | And   -> "&&"
+        | Or    -> "||"
+
+let rec string_of_expr = function
+          None -> ""
+        | Some Literal(l) -> string_of_int l
+        | Some Id(i) -> i
+        | Some Unop(op, e1) -> string_of_uop op ^ string_of_expr (Some e1)
+        | Some BoolLit(true) -> "true"
+        | Some BoolLit(false) -> "false"
+        | Some StringLit(s) ->  s
+        | Some Assign(id, e1) ->  id ^ " = " ^ string_of_expr (Some e1)
+        | Some Binop(e1, op, e2) -> string_of_expr (Some e1) ^ " " ^
+                string_of_op op ^ " " ^ string_of_expr (Some e2)
+        | Some Retrieve(id, e1) -> id ^ "[" ^ string_of_expr (Some e1) ^ "]"
+        | Some Call(id, act) -> id ^ "(" ^
+                String.concat ", "(List.map string_of_expr act) ^ ")"
+        | Some Array_Assign(id, e1, e2) -> id ^ "[" ^ string_of_expr (Some e1) ^
+                "]" ^ " = " ^ string_of_expr (Some e2)
+
+let string_of_opt_assn = function
+        None -> ""
+        | _ as exp -> " = " ^ string_of_expr exp
+
+let string_of_formal (typ, id, _) =
+        string_of_typ typ ^ " " ^ id
+
+let rec string_of_stmt = function
+          Expr(e1) -> string_of_expr (Some e1) ^ ";"
+        | Return(None) -> "return;"
+        | Return(e1) -> "return " ^ string_of_expr e1 ^ ";"
+        | Break -> "break;"
+        | Block(stmts) ->  "{\n" ^
+                String.concat "\n" (List.map string_of_stmt stmts) ^ "\n}\n"
+        | If(e1, s1, Block([])) -> "if (" ^ string_of_expr (Some e1) ^ ")\n" ^
+                string_of_stmt s1
+        | If(e1, s1, s2) -> "if (" ^ string_of_expr (Some e1) ^ ")\n" ^
+                string_of_stmt s1 ^ "else\n" ^ string_of_stmt s2
+        | For(typ, id1, id2, s1) -> "for (" ^ string_of_typ typ ^ id1 ^ " : " ^
+                id2 ^ ")" ^ string_of_stmt s1
+        | While(e1, s1) -> "while (" ^ string_of_expr (Some e1) ^ ") " ^
+                string_of_stmt s1
+        | Map(a1, f1) -> "map(" ^ a1 ^ ", " ^ f1 ^ ");\n"
+        | Filter(a1, f1) -> "filter(" ^ a1 ^ ", " ^ f1 ^ ");\n"
+        | Open(filename, delim) -> "open(" ^ filename ^ ", " ^ delim ^ ");\n"
+
+let string_of_vdecl (t, id, assn) =
+        string_of_typ t ^ " " ^ id ^ (string_of_opt_assn assn) ^ ";\n"
+
+let string_of_fdecl fdecl =
+        "func " ^ string_of_typ fdecl.typ ^ " " ^ fdecl.fname ^ " = " ^ "(" ^
+        String.concat ", " (List.map string_of_formal fdecl.formals) ^
+        ") => {\n" ^ String.concat "" (List.map string_of_vdecl fdecl.locals) ^
+        "\n" ^ String.concat "\n" (List.map string_of_stmt fdecl.body) ^ "\n}\n"
+
+let string_of_program (vars, funcs) =
+        String.concat "" (List.map string_of_vdecl vars) ^ "\n" ^
+        String.concat "\n" (List.map string_of_fdecl funcs)
+
---
+++ src/_build/semant.ml
@@ -0,0 +1,79 @@
+open Ast
+
+module StringMap = Map.Make(String)
+
+let check (vdec,fdec) = 
+
+  (* note, bind is a triple of typ, string and expr *)
+  let check_binds (kind : string) (binds : bind list) =
+    List.iter (function
+	(Void, b, c) -> raise (Failure ("illegal void " ^ kind ^ " " ^ b))
+      | _ -> ()) binds; 
+    let rec dups = function
+        [] -> ()
+      |	((_,n1,_) :: (_,n2,_) :: _) when n1 = n2 ->
+	  raise (Failure ("duplicate " ^ kind ^ " " ^ n1))
+      | _ :: t -> dups t
+    in dups (List.sort (fun (_,a,_) (_,b,_) -> compare a b) binds)
+in
+
+(* check variable declarations *)
+check_binds "variable declarations" vdec;
+
+(* begin checking functions *)
+
+(* function declaration for built-in FIRE functions - print, map, filter *)
+(* re-write for FIRE bind type? *)
+let built_in_func_decls = 
+    let add_bind map (name, rType) = StringMap.add name {
+        (* object between brackets is func_decl object? *)
+        typ = Void; (* all built in functions are of type void *)
+        fname = name;
+        formals = [(rType, "x", None)]; 
+        locals = []; (* empty list *)
+        body = []; (* empty list *)
+    } map 
+    (* REVISE following line !!!*)
+    in List.fold_left add_bind StringMap.empty [("print", String); ("map", String); ("filter", String)]
+in
+
+(* build up symbol table - global scope ONLY for now *)
+let add_func map fd=
+    let built_in_err = "function " ^ fd.fname ^ " may not be defined"
+    and dup_err = "duplicate function " ^ fd.fname
+    and make_err er = raise (Failure er)
+    and n = fd.fname (* Name of the function *)
+    in match fd with (* No duplicate functions or redefinitions of built-ins *)
+         _ when StringMap.mem n built_in_func_decls -> make_err built_in_err
+       | _ when StringMap.mem n map -> make_err dup_err  
+       | _ ->  StringMap.add n fd map 
+  in
+
+  (* collect all function names into symbol table *)
+let function_decls = List.fold_left add_func built_in_func_decls fdec
+
+in
+
+  (* Return a function from our symbol table *)
+let find_func s = 
+  try StringMap.find s function_decls
+  with Not_found -> raise (Failure ("unrecognized function " ^ s))
+in
+
+let _ = find_func "main" in (* Ensure "main" is defined *)
+
+(* code that vets functions below *)
+
+let check_function func =
+  (* Make sure no formals or locals are void or duplicates *)
+  check_binds "formal" func.formals;
+  check_binds "local" func.locals; 
+
+in (* placeholder in statement *)
+
+(* dud statement to resolve let...in express *)
+
+List.map check_function fdec;
+
+print_string "It worked \n"; 
+
