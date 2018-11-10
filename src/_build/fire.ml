c476b905e9d3d18aeb4e4bb9ebfbefb3d9718420
Lord-Left <sethosayher@gmail.com>
2018-11-10 15:57:57 -0500

    Began work on Sast.ml

---
+++ src/_build/fire.ml
@@ -0,0 +1,32 @@
+(* Top-level of the Fire compiler: scan & parse the input,
+   check the resulting AST, generate LLVM IR, and dump the module *)
+
+(*     ("-s", Arg.Unit (set_action Sast), "Print the SAST"); *)
+
+type action = Ast | Sast | LLVM_IR | Compile | SastDebug
+
+let _ =
+  let action = ref Compile in
+  let set_action a () = action := a in
+  let speclist = [
+    ("-a", Arg.Unit (set_action Ast), "Print the AST");
+    ("-s", Arg.Unit (set_action Sast), "Print semantically-checked AST");
+    ("-sd", Arg.Unit (set_action SastDebug), "Debug mode for Sast");
+    ("-l", Arg.Unit (set_action LLVM_IR), "Print the generated LLVM IR");
+    ("-c", Arg.Unit (set_action Compile),
+      "Check and print the generated LLVM IR (default)");
+  ] in
+  let usage_msg = "usage: ./fire.native [-a|-s|-l|-c] [file.fire]" in
+  let channel = ref stdin in
+  Arg.parse speclist (fun filename -> channel := open_in filename) usage_msg;
+
+  let lexbuf = Lexing.from_channel stdin in
+
+  let ast = Parser.program Scanner.token lexbuf in
+  match !action with
+    Ast -> print_string (Ast.string_of_program ast)
+    (*goal -> to eventually support the following logic: | _ -> let sast = Semant.check ast in *)
+    | Sast -> print_string "Sast Code is not complete. \n"
+    | SastDebug -> ignore(Semant.check ast)   (* ignore function takes an argument and forces a return of unit *)
+    | LLVM_IR -> print_string (Llvm.string_of_llmodule (Codegen.translate ast))
+
