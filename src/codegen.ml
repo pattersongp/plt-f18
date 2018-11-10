57d9cd8cba6c779c227f6fe00337b50c45c849b9
Graham Patterson <gpp2109@columbia.edu>
2018-11-10 13:02:02 -0500

    Parses user functions without body

--- src/codegen.ml
+++ src/codegen.ml
@@ -15,6 +15,7 @@ http://llvm.moe/ocaml/
 (* We'll refer to Llvm and Ast constructs with module names *)
 module L = Llvm
 module A = Ast
+open Ast (* This will change once we have Sast *)
 (* open Sast *)
 
 module StringMap = Map.Make(String)
@@ -23,26 +24,28 @@ module StringMap = Map.Make(String)
    throws an exception if something is wrong. *)
 let translate (globals, functions) =
   let context    = L.global_context () in
+  let the_module = L.create_module context "Fire" in
 
-  (* Add types to the context so we can use them in our LLVM code *)
+  (* ---------------------- Types ---------------------- *)
   let i32_t      = L.i32_type    context
   and i8_t       = L.i8_type     context
   and i1_t       = L.i1_type     context
   and void_t     = L.void_type   context
-  (* Create an LLVM module -- this is a "container" into which we'll
-     generate actual code *)
-  and the_module = L.create_module context "Fire" in
+  in
 
-  let pointer_t  = L.pointer_type i8_t in
+  let string_t = L.pointer_type i8_t
+  and i32_ptr_t = L.pointer_type i32_t
+  and i8_ptr_t = L.pointer_type i8_t in
 
   (* Convert Fire types to LLVM types *)
   let ltype_of_typ = function
       A.Int   -> i32_t
     | A.Bool  -> i1_t
     | A.Void  -> void_t
-(*     | A.String -> pointer_t *)
+    | A.String -> string_t
   in
 
+(* need to revisit this *)
   let rec global_expr = function
       A.Literal i -> i
     | A.BoolLit b -> (if b then 1 else 0)
@@ -68,5 +71,25 @@ let translate (globals, functions) =
                 in L.const_pointer_null (ltype_of_typ typ) v'
 *)
       in StringMap.add n (L.define_global n init the_module) m in
-    List.fold_left global_var StringMap.empty globals in  the_module
+    List.fold_left global_var StringMap.empty globals in
+
+  (* ---------------------- External Functions ---------------------- *)
+  let print_t : L.lltype =
+    L.var_arg_function_type i32_t [| L.pointer_type i8_t |] in
+  let print_func : L.llvalue =
+    L.declare_function "print" print_t the_module in
+
+  (* ---------------------- User Functions ---------------------- *)
+  let function_decls : (L.llvalue * func_decl) StringMap.t =                    (* Will become sfunc_decl *)
+    let function_decl m fdecl =
+      let name = fdecl.fname                                                    (* will become sfname *)
+      and formal_types =
+        Array.of_list (List.map (fun (t,_,_) -> ltype_of_typ t) fdecl.formals)  (* will become sformals*)
+      in
+        let ftype = L.function_type (ltype_of_typ fdecl.typ) formal_types in    (* will become styp *)
+          StringMap.add name (L.define_function name ftype the_module, fdecl) m
+        in
+    List.fold_left function_decl StringMap.empty functions
+  in
+  the_module
 
