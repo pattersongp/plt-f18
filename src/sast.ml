f0c5f3ab2622541feb7dd476dd8f517499120e25
frank spano <fspano@tripcents.co>
2018-11-15 17:53:09 -0500

    added sast

--- src/sast.ml
+++ src/sast.ml
@@ -10,10 +10,9 @@ and sx =
   | SId of string
   | SBinop of sexpr * op * sexpr
   | SUnop of uop * sexpr
-  | SAssign of string * sexpr
   | SRetrieve of string * sexpr
-  | SArray_Assign of string * sexpr * sexpr
   | SCall of string * sexpr list
+  | SNoexpr
 
 type sstmt = 
     SBlock of sstmt list
@@ -26,12 +25,14 @@ type sstmt =
   | SFilter of string * string
   | SOpen of string * string
   | SBreak
+  | SVdecl of typ * string * expr
+  | SAssign of string * sexpr
+  | SArray_Assign of string * sexpr * sexpr
 
 type sfunc_decl = {
     styp : typ;
     sfname : string;
     sformals : bind list;
-    slocals : bind list;
     sbody : stmt list;
 }
 
@@ -48,12 +49,10 @@ let rec string_of_sexpr (t, e) =
   | SBinop(e1, o, e2) ->
       string_of_sexpr e1 ^ " " ^ string_of_op o ^ " " ^ string_of_sexpr e2
   | SUnop(o, e) -> string_of_uop o ^ string_of_sexpr e
-  | SAssign(v, e) -> v ^ " = " ^ string_of_sexpr e
   | SCall(f, el) ->
       f ^ "(" ^ String.concat ", " (List.map string_of_sexpr el) ^ ")"
   | SRetrieve(id, e1) -> id ^ "[" ^ string_of_expr e1 ^ "]"
-  | SArray_Assign(id, e1, e2) -> id ^ "[" ^ string_of_expr e1 ^
-                "]" ^ " = " ^ string_of_expr e2
+  | SNoexpr -> ""
 
 let rec string_of_sstmt = function
     SBlock(stmts) ->
@@ -72,15 +71,17 @@ let rec string_of_sstmt = function
   | SFilter(a1, f1) -> "filter(" ^ a1 ^ ", " ^ f1 ^ ");\n"
   | SOpen(filename, delim) -> "open(" ^ filename ^ ", " ^ delim ^ ");\n"
   | Break -> "break;"
+  | SArray_Assign(id, e1, e2) -> id ^ "[" ^ string_of_expr e1 ^
+                "]" ^ " = " ^ string_of_expr e2
+  | SAssign(v, e) -> v ^ " = " ^ string_of_sexpr e
+  | Vdecl(t, id, e) -> string_of_vdecl (t, id, e)
 
 let string_of_sfdecl fdecl =
   string_of_typ fdecl.styp ^ " " ^
   fdecl.sfname ^ "(" ^ String.concat ", " (List.map snd fdecl.sformals) ^
   ")\n{\n" ^
-  String.concat "" (List.map string_of_vdecl fdecl.slocals) ^
   String.concat "" (List.map string_of_sstmt fdecl.sbody) ^
   "}\n"
 
 let string_of_program (vars, funcs) =
-        String.concat "" (List.map string_of_vdecl vars) ^ "\n" ^
         String.concat "\n" (List.map string_of_fdecl funcs)
