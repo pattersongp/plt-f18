c41bd0cd27c9d6b829df3427d196db0bee161ad5
Oi I Chan <oiichan@dyn-129-236-227-169.dyn.columbia.edu>
2018-11-20 15:57:17 -0500

    create cpp array template that do absolutely nothing and create c array template as well

---
+++ src/arrlib.cpp
@@ -0,0 +1,7 @@
+#include <arrlib.h>
+
+Node::Node(void k1, void k2){
+    key1 = k1;
+    key2 = k2;
+}
+
---
+++ src/arrlib.h
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+
+using namespace std;
+
+class Array{
+public:
+    //no default constructors
+    Array(void,void);
+private:
+    void key1;
+    void key2;
+}
