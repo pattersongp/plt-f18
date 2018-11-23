75f1ad4a418a0336bb3fb09ae7c27259074b0748
Oi I Chan <oiichan@Ois-MacBook-Pro.local>
2018-11-23 15:45:25 -0500

    trying c++

--- src/arrlib.cpp
+++ src/arrlib.cpp
@@ -1,7 +1,29 @@
-#include <arrlib.h>
+//Array Library C++ for Fire Language
+#include <cstdlib>
+#include <iostream>
+#include "arrlib.h"
 
-Node::Node(void k1, void k2){
-    key1 = k1;
-    key2 = k2;
+using namespace std;
+
+int main(){
+    
 }
 
+template<typename AnyType>
+void Array::add(AnyType data1, AnyType data2){
+    struct Node newNode = malloc(sizeof(struct Node));
+    
+    //check malloc
+    if(newNode == NULL){
+        thow "melloc return NULL";
+        exit(1);
+    }
+    
+    //assign new node's all property
+    newNode->data1 = data1;
+    newNode->data2 = data2;
+    newNode->next = this->head;    //pointing to same as head's
+    
+    //assign head
+    this->head = newNode;
+}
