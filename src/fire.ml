9ba50e7206db42a26b0c413afdbf9a3a2683acd7
Graham Patterson <pattersongp.usmc@gmail.com>
2018-11-27 21:49:38 -0500

    Frank tmp2 (#53)
    * Added -s flag
    
    * Added detection of void functions and duplicate name functions to semant.ml
    
    * Added some comments re: void & dup detection in semant.ml
    
    * Added beginnings of symbol table function.
    
    * Revised declaration symbol table function; have to find way to express expr in order for function to work in semant.ml.
    
    * Revised semant.ml and built-in function code.
    
    * change arraydecl to array start semant changes
    
    * changed assign and array assign to stmnt in AST
    
    * added vdecl to AST under stmnt
    
    *  whelo
    
    * adds fix that requires Block body
    
    * added call scafolding to rec expr
    
    * sast v1
    
    * added sast
    
    * fixed compilation errors in sast, corrected several pretty printing errors in sast
    
    * modifed fire.ml to reflect new semant.check params
    
    * modified semant after refactors, compiles up to check_function
    
    * fixed incorrect param in symbols, semant compiling throught start of rec expr
    
    * corrected ast.op token names in semant, compiles through binop/unop
    
    * semant compiling through function call semantic check in expr
    
    * finished rec expr and check_bool, compiles up to check_stmt
    
    * added SStringLit to pretty printer
    
    * replaced arg in check_binds
    
    * partial merge
    
    * vdecl
    
    * compiling except for 1 line
    
    * semant compiling
    
    * changed sast and fire to build sast
    
    * semant compiles pre check_stmnt refactor
    
    * fucking around
    
    * attempts to pass enviroment through check_stmt calls
    
    * logically sound fixing syntax errors
    
    * semant compiled, needs testing

--- src/fire.ml
+++ src/fire.ml
@@ -3,17 +3,18 @@
 
 (*     ("-s", Arg.Unit (set_action Sast), "Print the SAST"); *)
 
-(* type action = Ast | Sast | LLVM_IR | Compile *)
-type action = Ast | LLVM_IR | Compile
+type action = Ast | Sast | LLVM_IR | Compile | SastDebug
+
 
 let _ =
   let action = ref Compile in
   let set_action a () = action := a in
   let speclist = [
     ("-a", Arg.Unit (set_action Ast), "Print the AST");
+    ("-s", Arg.Unit (set_action Sast), "Print semantically-checked AST");
+    ("-sd", Arg.Unit (set_action SastDebug), "Debug mode for Sast");
     ("-l", Arg.Unit (set_action LLVM_IR), "Print the generated LLVM IR");
-(*     ("-c", Arg.Unit (set_action Compile), *)
-(*       "Check and print the generated LLVM IR (default)"); *)
+    ("-c", Arg.Unit (set_action Compile), "Check and print the generated LLVM IR (default)");
   ] in
   let usage_msg = "usage: ./fire.native [-a|-s|-l|-c] [file.fire]" in
   let channel = ref stdin in
@@ -22,8 +23,12 @@ let _ =
   let lexbuf = Lexing.from_channel stdin in
 
   let ast = Parser.program Scanner.token lexbuf in
+  let sast = Semant.check ast in
   match !action with
     Ast -> print_string (Ast.string_of_program ast)
+    (*goal -> to eventually support the following logic: | _ -> let sast = Semant.check ast in *)
+    | Sast -> print_string "Sast Code is not complete. \n"
+    | SastDebug -> print_string (Sast.string_of_sprogram sast)   (* ignore function takes an argument and forces a return of unit *) 
     | LLVM_IR -> print_string (Llvm.string_of_llmodule (Codegen.translate ast))
     | _ -> ()
 
