0e443202fee23643e8ac36a031378d35d02831cf
Oi I Chan <oiichan@dyn-129-236-226-111.dyn.columbia.edu>
2018-11-29 17:28:13 -0500

    get rid of strcmp, add type flg,keep track of  array in array size

--- src/arrlib.c
+++ src/arrlib.c
@@ -2,8 +2,6 @@
 #include <stdlib.h>
 #include <sys/types.h>
 #include <stdio.h>
-#include <string.h>
-
 #include "arrlib.h"
 
 typedef struct Node {
@@ -16,21 +14,70 @@ typedef struct Array {
     struct Node *head;
     size_t size_key;
     size_t size_value;
-    size_t length;
+    int length;
+    char type_key;
+    char type_value;
 } Array;
+
+char checktypefromcodegen (int codegensize){
+    //s = string, i = int, a = array, b = bool
+    char type;
+    switch (codegensize) {
+        case 8:
+            type = 'i';
+            break;
+        case 7:
+            type = 's';
+            break;
+        case 4:
+            type = 'b';
+            break;
+        default:
+            type = 'a';
+            break;
+    }
+    return type;
+}
+
+int size(char type){
+    int realsize;
+    switch (type) {
+        case 'i':
+            realsize = 8;
+            break;
+        case 's':
+            realsize = 8;
+            break;
+        case 'b':
+            realsize = 4;
+            break;
+        default:
+            realsize = 0;
+            break;
+    }
+    return realsize;
+}
+
 
 struct Array *initArray(int size_k, int size_v) {
     struct Array *ar = malloc(sizeof(struct Array));
     ar->head = 0;
-    ar->size_key = size_k;
-    ar->size_value = size_v;
     ar->length = 0;
+    
+    ar->type_key = checktypefromcodegen(size_k);
+    ar->type_value = checktypefromcodegen(size_v);
+    
+    ar->size_key = size(ar->type_key);
+    ar->size_value = size(ar->type_value);
+    
+    //printf("Type value: %c\n", ar->type_value);
+    //printf("Size value: %d\n", (int)ar->size_value);
+    
     return ar;
 }
 
-struct Node *findNode(struct Array *array, const void *dataSought,
-	int (*compar)(const void *, const void *)) {
-
+struct Node *findNode(struct Array *array, const void *dataSought, int (*compar)(const void *, const void *)) {
+    
     struct Node *node = array->head;
     while (node) {
         if (compar(dataSought, node->data1) == 0) { return node; }
@@ -39,71 +86,109 @@ struct Node *findNode(struct Array *array, const void *dataSought,
     return NULL;
 }
 
+int compareInt(const void *data1, const void *data2)
+{
+    int a = (int)data1;
+    int b = (int)data2;
+    if (a == b)
+        return 0;
+    else
+        return 1;
+}
+
+int compareString(const void *data1, const void *data2)
+{
+    if (*(char *)data1 == *(char *)data2)
+        return 0;
+    else
+        return 1;
+}
+
+int setsizeofarrayinarray(const struct Array *array){
+    int len = array->length;
+    int ks = (int)array->size_key;
+    int vs = (int)array->size_value;
+    int arraysize = (ks+vs)*len;
+    return arraysize;
+}
+
 void add(struct Array *array, void *data1, void *data2) {
     struct Node *node;
-
-	// will need to eventually change this to be another comparator function
-    node = findNode(array, data1, (int (*)(const void *, const void *))&strcmp);
-
+    
+    // compare function
+    if (array->type_key == 'i') {
+        node = findNode(array, data1, &compareInt);
+    }else{
+        node = findNode(array, data1, &compareString);
+        //node = findNode(array, data1, (int (*)(const void *, const void *))&strcmp);
+    }
+    
     if (node != NULL) {
         node->data2 = data2;
+        if (array->type_value == 'a') {
+            Array *arr = (struct Array*)node->data2;
+            arr->size_value = setsizeofarrayinarray(arr);
+        }
     } else {
         //create a new node malloc but no free
         struct Node *newNode = malloc(sizeof(struct Node));
-
+        
         //check malloc
         if(newNode == NULL) {
             perror("malloc return NULL");
             exit(1);
         }
-
+        
         //assign new node's all property
         newNode->data1 = data1;
         newNode->data2 = data2;
         newNode->next = array->head;    //pointing to same as head's
-
+        
         //assign head
         array->head = newNode;
         node = newNode;
-
+        
         //add length
         array->length = array->length + 1;
+        printf("Array Length: ");
+        printf("%d \n",array->length);
     }
 }
 
-void *get(struct Array *array, const void *dataSought) {
+void *get(struct Array *array, const void *key) {
     //Lookup key
     struct Node *node;
-    node = findNode(array, dataSought, (int (*)(const void *, const void *))&strcmp);
-    char *value = node->data2;
-
-    return value;
+    // compare function
+    if (array->type_key == 'i') {
+        node = findNode(array, key, &compareInt);
+    }else{
+        node = findNode(array, key, &compareString); //node = findNode(array, data1, (int (*)(const void *, const void *))&strcmp);
+    }
+    
+    return node->data2;
 }
 
-#if 0
-void set(struct Array *array, const void *dataSought, char *valueSet) {
-    //lookup key
-    struct Node *node;
-    node = findNode(array, dataSought, (char (*)(const void *, const void *))&strcmp);
-    node->data2 = valueSet;
-}
-#endif
-
-#ifdef BUILD_TEST
+//#ifdef BUILD_TEST
 int main() {
     //No default constructor
-    Array *arr = initArray(8,8);
-
+    Array *arr;
+    arr = initArray(7,8);   //init(string,int);
+    
     //Adding an element
-    add(arr,"Age","17");
-    add(arr,"Birthday","");
-
+    add(arr,"Age",17);
+    add(arr,"Birthday",1959);
+    
+    //Array *arr2;
+    //arr2 = initArray(7,10);
+    //add(arr, "1", arr2);
+    
     //Retrieve a value
-    char *c = get(arr,"Age");
-    printf("%s\n", c);
-
+    int c = (int)get(arr,"Age");
+    printf("Age: %d\n", c);
+    
     return 0;
 }
-#endif
+//#endif
+
 
 
