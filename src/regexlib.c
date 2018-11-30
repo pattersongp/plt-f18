3f79f1f2d82332d6c1af45f1401742dc7b570827
Graham Patterson <gpp2109@columbia.edu>
2018-11-29 21:42:17 -0500

    Adds a library call to parse the matches string

--- src/regexlib.c
+++ src/regexlib.c
@@ -30,12 +30,45 @@ int regex_compare(char *regex, char *operand) {
 		printf("library says there is NOT a match\n");
 	}
 #endif
+
+	regfree(&preg);
 	if(ret == 0) return 1;
 	return 0;
+}
+
+char *regex_grab(char *regex, char *operand) {
+	char clean_regex[strlen(regex)-2];
+	char clean_operand[strlen(operand)-2];
+	int i;
+	regex_t preg;
+
+	if(regcomp(&preg, (const char *)regex, 0) != 0) {
+		printf("regcomp() failed");
+	}
+
+
+	regmatch_t pmatch[2];
+	int ret = regexec((const regex_t *)&preg, operand, 1, pmatch, 0);
+
+	char *result = (char*)malloc(pmatch[0].rm_eo - pmatch[0].rm_so);
+	strncpy(result, &operand[pmatch[0].rm_so], pmatch[0].rm_eo - pmatch[0].rm_so);
+
+#ifdef DEBUG
+	printf("matched: \"%s\"\n at %lld to %lld\n", result, pmatch[0].rm_so, pmatch[0].rm_eo - 1);
+
+	if(ret == 0) {
+		printf("library says there is a match\n");
+	} else {
+		printf("library says there is NOT a match\n");
+	}
+#endif
+	if(ret == 0) { return result; }
+	return "";
 }
 
 #ifdef BUILD_TEST
 int main() {
 	regex_compare("[:alpha:]", "4115hello4115");
+	regex_grab("hello", "4115hello4115");
 }
 #endif
